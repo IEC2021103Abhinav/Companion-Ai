b5a22b17118b86b794b9b555bb5d7c9c91b31687
IEC2021103Abhinav <iec2021103@iita.ac.in>
2024-09-02 13:58:38 +0530

    ImageUpload Cloudinary Commit

--- prisma/migrations/20240829201147_init/migration.sql
+++ prisma/migrations/20240829201147_init/migration.sql
@@ -5,3 +5,7 @@ CREATE TABLE "Category" (
 
     CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
 );
+
+CREATE INDEX name_fulltext_idx ON "Companion" USING GIN (to_tsvector('english', "name"));
+-- as @@fulltext([name]) is not working in schema.prisma , so we can add here  
+
